b7d44903ff3c734475181c087ffceb57d80ac314
조용필 <78397393+bbie-6772@users.noreply.github.com>
2024-11-25 12:46:52 +0900

    Docs: 필요없는 package 제거

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
 import dotenv from 'dotenv'
 import express from 'express'
-import expSession from 'express-session'
-import expSQLSession from 'express-mysql-session'
+import accountRouter from './routes/account.router.js'
 
 // .env 정보 가져오기
 dotenv.config();
 
 const app = express();
-const PORT = 3020
+const PORT = 3030
 
-const MySQLStore = expSQLSession(expSession)
+//json 형태의 요청 body 인식
+app.use(express.json());
 
-const sessionStor = new MySQLStore ({
-    user: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    database: process.env.DATABASE_NAME,
-    // 만료기한 (=1일)
-    expiration: 1000 * 60 * 60 * 24,
-    createDatabaseTable: true
-})
+app.use('/api', accountRouter)
+
+app.listen(PORT, () => {
+    console.log(PORT, '포트로 서버가 열렸어요!');
+});
