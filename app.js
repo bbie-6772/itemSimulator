817e1cef5548a9b17b89379213a30e1e1a477eec
조용필 <78397393+bbie-6772@users.noreply.github.com>
2024-11-26 15:29:54 +0900

    Feat: 에러 처리
    일부 예상치 못한 에러 발생 시,에도 서버 유지 가능케 구현

--- app.js
+++ app.js
@@ -16,3 +16,8 @@ app.use('/api', [accountRouter, characterRouter, itemRouter])
 app.listen(PORT, () => {
     console.log(PORT, '포트로 서버가 열렸어요!');
 });
+
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
