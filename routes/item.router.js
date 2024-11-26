1915169385ca4fbfe27592805e42fdaa27465e25
조용필 <78397393+bbie-6772@users.noreply.github.com>
2024-11-26 15:33:42 +0900

    Feat: 아이템 수정 API 구현
    + 기존에 있던 아이템 생성 API 약간 리팩토링

--- routes/item.router.js
+++ routes/item.router.js
@@ -10,7 +10,7 @@ dotenv.config();
 const router = express.Router();
 
 // 아이템 생성 API
-router.post('/items-add', async (req,res,next) => { 
+router.post('/items', async (req,res,next) => { 
     //입력값 확인
     const {item_code, item_name, item_stat, item_price} = req.body;
     //아이템 생성용 변수
@@ -49,23 +49,22 @@ router.post('/items-add', async (req,res,next) => {
         item = await prisma.itemTable.create({
             data: {
                 itemCode: +item_code,
+                price: +item_price,
                 name: item_name,
-                health: health,
-                power: power,
-                price: +item_price
+                health,
+                power
             }
         })
     } else {
         item = await prisma.itemTable.create({
             data: {
+                price: +item_price,
                 name: item_name,
-                health: health,
-                power: power,
-                price: +item_price
+                health,
+                power
             }
         })
     }
-
     return res
         .status(201)
         .json({
@@ -75,6 +74,51 @@ router.post('/items-add', async (req,res,next) => {
                     item_stat:  { health: item.health, power: item.power },
                     item_price: item.price
               }
+        })
+})
+
+// 아이템 수정 API
+router.patch('/items/:itemCode', async (req,res,next) => {
+    const {itemCode} = req.params;
+    let {item_name,item_stat} = req.body;
+    let health, power
+    // 변경사항이 없을 시(입력이 없을 시)
+    if (!(item_name || item_stat)) return res
+        .status(404)
+        .json({ errorMessage: "아이템에 변경사항이 존재하지 않습니다" })
+    // 아이템 존재 확인
+    const item = await prisma.itemTable.findFirst({ where: { itemCode: +itemCode } })
+    if (!item) return res
+        .status(404)
+        .json({ errorMessage: `<itemCode> ${itemCode}번의 아이템이 존재하지 않습니다` })
+    // 인수 확인 및 없을 시 기본값 유지
+    item_stat ? { health, power } = item_stat : 0
+    item_name = item_name ? item_name : item.name
+    health = health ? +health : item.health
+    power = power ? +power : item.power
+
+    // 아이템 수정
+    await prisma.itemTable.update({
+        data: {
+            name: item_name,
+            health,
+            power
+        },  
+        where : { itemCode: +itemCode }
+    })
+    // 수정된 아이템 정보
+    const newItem = await prisma.itemTable.findFirst({ where: { itemCode: +itemCode } })
+    
+    return res
+        .status(201)
+        .json({
+            message: `아이템 ${item.name}(이)가 수정되었습니다.`,
+            data: {
+                item_code: newItem.itemCode,
+                item_name: newItem.name,
+                item_stat: { health: newItem.health, power: newItem.power },
+                item_price: newItem.price
+            }
         })
 })
 
