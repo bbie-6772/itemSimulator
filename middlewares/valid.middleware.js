a848c43d4cdc9903a21a2dd5d18229ad0192402c
조용필 <78397393+bbie-6772@users.noreply.github.com>
2024-11-28 14:29:34 +0900

    Refactor: character.router 유효성 평가 간소화
    유효성 평가 미들웨어를 생성하여 같은기능은 간소화되게 리팩토링 진행하였음

--- middlewares/valid.middleware.js
+++ middlewares/valid.middleware.js
@@ -1,7 +1,7 @@
 import { prisma } from '../prisma/index.js'
 
-//유효성 평가 미들웨어
-const signvaild = async function (req, res, next) {
+//account router 유효성 평가 미들웨어
+const signVaild = async function (req, res, next) {
     try {
         const { id, password, passwordCheck } = req.body;
         const path = req.path;
@@ -12,7 +12,7 @@ const signvaild = async function (req, res, next) {
         // 비밀번호가 6글자 이상인지 확인
         if (!/\b.{6,}/.test(password)) throw new Error("비밀번호는 6글자 이상으로 작성해주세요", { cause: 400 })
 
-        // 회원가입 경우
+        // 회원가입
         if (path === "/sign-up") {
             // 아이디 중복 확인
             const isExitUser = await prisma.accounts.findFirst({ where: { userId: id } })
@@ -23,7 +23,7 @@ const signvaild = async function (req, res, next) {
 
             //비밀번호 확인과 일치하는지
             if (!(password === passwordCheck)) throw new Error("비밀번호가 일치하지 않습니다", { cause: 401 })
-        // 로그인의 경우
+        // 로그인
         } else if (path === "/sign-in") {
             // 아이디가 없을 시
             const account = await prisma.accounts.findFirst({ where: { userId: id } })
@@ -36,8 +36,42 @@ const signvaild = async function (req, res, next) {
         return res
             .status(err.cause)
             .json({ errorMessage: err.message })
-
     }
 }
 
-export { signvaild }
+//character router 유효성 평가 미들웨어
+const charVaild = async function (req, res, next) {
+    try {
+        const { name } = req.body;
+        const { charId } = req.params;
+        const { path, user, method} = req;
+
+        // 캐릭터 생성 
+        if (path === "/characters") {
+            // 이름 존재 유무
+            if (!name) throw new Error("캐릭터 이름 <name>을 입력해주세요", { cause: 400 })
+            // 이름 중복 여부 
+            const isExitChar = await prisma.characters.findFirst({ where: { name } })
+            if (isExitChar) throw new Error("이미 존재하는 이름입니다", { cause: 409 })
+        // 캐릭터 삭제/조회
+        } else if (path === `/characters/${charId}`) {
+            // 캐릭터 id 확인
+            if (!charId) throw new Error("선택할 <캐릭터 ID>를 URL에 숫자로 입력해주세요.", { cause: 409 })
+            
+            // 캐릭터 존재여부 확인
+            const character = await prisma.characters.findFirst({ where: { charId: +charId } })
+            if (!character) throw new Error(`<character_id> ${charId} 에 해당하는 캐릭터가 존재하지 않습니다.`, { cause: 404 })
+                
+            // 계정에 귀속된 캐릭터가 맞는지 확인(삭제의 경우만)
+            if (method === "DELETE" && character.accountId !== user.accountId) throw new Error("본 계정이 소유한 캐릭터가 아닙니다.", { cause: 401 })
+        }
+        next()
+        //던진 오류들 확인해서 반환
+    } catch (err) {
+        return res
+            .status(err.cause)
+            .json({ errorMessage: err.message })
+    }
+}
+
+export { signVaild, charVaild }
--- routes/character.router.js
+++ routes/character.router.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import { Prisma } from '@prisma/client';
 import { prisma } from '../prisma/index.js';
+import { charVaild } from '../middlewares/valid.middleware.js'
 import { authMiddleware, decodeMiddlware } from '../middlewares/auth.middleware.js';
 
 dotenv.config();
@@ -9,19 +10,11 @@ dotenv.config();
 const router = express.Router();
 
 // 캐릭터 생성 API
-router.post('/characters', authMiddleware , async (req,res,next) => {
+router.post('/characters', authMiddleware, charVaild , async (req,res,next) => {
     //인증 후 사용자 아이디 할당
     const user = req.user;
     const {name} = req.body;
-    // 입력값 유효성 검사 
-    if (!name) return res
-        .status(400)
-        .json({ errorMessage: "데이터 형식이 올바르지 않습니다."})
-    // 이름 중복 여부 
-    const isExitChar = await prisma.characters.findFirst({ where: { name } })
-    if (isExitChar) return res
-        .status(409)
-        .json({ errorMessage: "이미 존재하는 이름입니다" })
+
     //트랜잭션 사용으로 캐릭터/인벤토리/장비 동시 생성
     const [character, inventory, equipment] = await prisma.$transaction( async (tx) => {
         //캐릭터 생성
@@ -61,22 +54,9 @@ router.post('/characters', authMiddleware , async (req,res,next) => {
 })
 
 // 캐릭터 삭제 API
-router.delete('/characters/:charId', authMiddleware, async (req,res,next) => {
+router.delete('/characters/:charId', authMiddleware, charVaild, async (req,res,next) => {
     const {charId} = req.params;
     const user = req.user;
-    // 입력 확인
-    if (!charId) return res
-        .status(400)
-        .json({ errorMessage: "데이터 형식이 올바르지 않습니다."})
-    const character = await prisma.characters.findFirst({where: { charId: +charId }})
-    // 캐릭터 존재여부 확인
-    if (!character) return res
-        .status(404)
-        .json({ errorMessage: `<character_id> ${charId} 에 해당하는 캐릭터가 존재하지 않습니다.` })
-    // 계정에 귀속된 캐릭터가 맞는지 확인
-    if (character.accountId !== user.accountId) return res
-        .status(401)
-        .json({ errorMessage: "본 계정이 소유한 캐릭터가 아닙니다."})
 
     await prisma.characters.delete({ where: { charId: +charId }})
     
@@ -86,19 +66,11 @@ router.delete('/characters/:charId', authMiddleware, async (req,res,next) => {
 })
 
 // 캐릭터 상세 조회 API
-router.get('/characters/:charId', decodeMiddlware, async (req,res,next) => {
+router.get('/characters/:charId', decodeMiddlware, charVaild, async (req,res,next) => {
     const {charId} = req.params;
     const user = req.user;
-    // 입력 확인
-    if (!charId) return res
-        .status(400)
-        .json({ errorMessage: "데이터 형식이 올바르지 않습니다." })
+
     const character = await prisma.characters.findFirst({ where: { charId: +charId } })
-    // 캐릭터 존재여부 확인
-    if (!character) return res
-        .status(404)
-        .json({ errorMessage: `<character_id> ${charId} 에 해당하는 캐릭터가 존재하지 않습니다.` })
-    // 계정소유자가 맞는지 확인
     if (character.accountId === (user ? +user.accountId : 0)) {
         return res
             .status(200)
@@ -123,18 +95,8 @@ router.get('/characters/:charId', decodeMiddlware, async (req,res,next) => {
 })
 
 // 동전줍기 API
-router.get('/money/:charId', authMiddleware, async (req,res,next) => {
+router.get('/money/:charId', authMiddleware, charVaild, async (req,res,next) => {
     const {charId} = req.params
-    const user = req.user
-    // 캐릭터 존재여부 확인
-    const character = await prisma.characters.findFirst({ where: { charId: +charId } })
-    if (!character) return res
-        .status(404)
-        .json({ errorMessage: `<character_id> ${charId} 에 해당하는 캐릭터가 존재하지 않습니다.` })
-    // 계정에 귀속된 캐릭터가 맞는지 확인
-    if (character.accountId !== user.accountId) return res
-        .status(401)
-        .json({ errorMessage: "본 계정이 소유한 캐릭터가 아닙니다." })
 
     //돈 랜덤 획득
     const amonut = Math.round(Math.random() * 10) * 100
